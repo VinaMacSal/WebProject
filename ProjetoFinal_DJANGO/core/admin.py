6a37b10e33d70cd343a2731708fae6a865728478
VinaMacSal <vinicius817@gmail.com>
2019-02-24 20:12:55 -0300

    Mov Rotativo e Mensalista

--- ProjetoFinal_DJANGO/core/admin.py
+++ ProjetoFinal_DJANGO/core/admin.py
@@ -4,22 +4,31 @@ from .models import (Marca,
 	Pessoa, 
 	Parametros, 
 	MovRotativo,
+	Mensalista,
+	MovMensalista,
 	)
 
 
 class MovRotativoAdmin(admin.ModelAdmin):
-	def mostrar_veiculo(self, obj):
-		return obj.veiculo.placa + " - " + str(obj.veiculo.marca)
-	mostrar_veiculo.admin_order_field = 'veiculo'
-	mostrar_veiculo.short_description = 'Veiculo'
+	#def mostrar_veiculo(self, obj):
+		#return obj.veiculo.placa + " - " + str(obj.veiculo.marca)
+	
+	#mostrar_veiculo.admin_order_field = 'veiculo'
+	#mostrar_veiculo.short_description = 'Veiculo'
 
-	list_display = ('mostrar_veiculo', 'checkin', 'checkout', 
+	list_display = ('veiculo', 'checkin', 'checkout', 
 		"valor_hora", 'pago', 'total',
 		'horas_total')
+
+
+class MovMensalistaAdmin(admin.ModelAdmin):
+	list_display = ('mensalista', 'dt_pgto', 'total')
 
 
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
 admin.site.register(MovRotativo, MovRotativoAdmin)
--- ProjetoFinal_DJANGO/core/models.py
+++ ProjetoFinal_DJANGO/core/models.py
@@ -52,5 +52,17 @@ class MovRotativo(models.Model):
 	def total(self):
 		return self.valor_hora * self.horas_total()
 
+
+class Mensalista(models.Model):
+	veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+	inicio = models.DateField()
+	valor_mes = models.DecimalField(max_digits=6, decimal_places=2) 
+
 	def __str__(self):
-		return self.veiculo.placa
+		return str(self.veiculo)
+
+
+class MovMensalista(models.Model):
+	mensalista = models.ForeignKey(Mensalista, on_delete=models.CASCADE)
+	dt_pgto = models.DateField()
+	total = models.DecimalField(max_digits=6, decimal_places=2)
