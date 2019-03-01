a5d5fa0c53e1347b196410095b92c8149ff2aec4
VinaMacSal <vinicius817@gmail.com>
2019-02-28 23:22:57 -0300

    Adicionado BootStrap

--- ProjetoFinal_DJANGO/estacionamento/settings.py
+++ ProjetoFinal_DJANGO/estacionamento/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'core',
 ]
 
@@ -124,3 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
