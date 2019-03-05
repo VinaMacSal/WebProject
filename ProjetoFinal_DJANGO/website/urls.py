c179bf722ef4f44439288ebf4a534cbd972ebf1e
VinaMacSal <vinicius817@gmail.com>
2019-03-05 18:24:27 -0300

    Estilizando o WebSite

--- ProjetoFinal_DJANGO/website/urls.py
+++ ProjetoFinal_DJANGO/website/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import home
+from .views import (
+	home,
+	planos,
+	servicos,
+	contato,
+	sobre,
+	)
 	
-
 urlpatterns = [
     path('', home, name="website_home"),
+    path('planos/', planos, name="website_planos"),
+    path('servicos/', servicos, name="website_servicos"),
+    path('contato/', contato, name="website_contato"),
+    path('sobre/', sobre, name="website_sobre"),
     ]
